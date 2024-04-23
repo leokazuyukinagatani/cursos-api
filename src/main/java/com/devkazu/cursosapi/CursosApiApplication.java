09df999295ef4b425d440cf09af619a5afd87e57
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-04-23 07:20:07 -0300

    init project

---
+++ src/main/java/com/devkazu/cursosapi/CursosApiApplication.java
@@ -0,0 +1,13 @@
+package com.devkazu.cursosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosApiApplication.class, args);
+	}
+
+}
